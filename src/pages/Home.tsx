ba58e6b954c3fa0784b947b9c5fb45563c40a492
Gautham Vanjre <vanjregautham@gmail.com>
2023-10-10 20:11:49 +0530

    added input search bar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,36 @@ import React from "react";
 import Navbar from "../components/navbar";
 
 const Home = () => {
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  /**
+   * When the user types in the search box, the value of the search box is set to the state of the
+   * search term.
+   * @param event - The event object is a JavaScript event that is sent to an element when an event
+   * occurs on the element.
+   */
+  const handleSearchTermChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
     <>
       <Navbar home={true} wishlist={false} />
+
+      <section className="search-component ">
+        <div className="search-input-container">
+          <label className="search-input-field-container">
+            <input
+              onChange={handleSearchTermChange}
+              type="text"
+              placeholder="Articles names or keywords"
+            />
+          </label>
+          <span className="bg-black p-2 text-white">Search</span>
+        </div>
+      </section>
     </>
   );
 };
