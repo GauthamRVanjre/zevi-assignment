a3f1630900098f21fef9c72fa6dd36d5bad55260
Gautham Vanjre <vanjregautham@gmail.com>
2023-10-11 19:24:08 +0530

    deleted navbar and improved 404 page

--- src/pages/NoPage.tsx
+++ src/pages/NoPage.tsx
@@ -1,10 +1,15 @@
 import React from "react";
-import Navbar from "../components/navbar";
+import { Link } from "react-router-dom";
 
 const NoPage = () => {
   return (
     <>
-      <Navbar home={false} wishlist={false} />
+      <body className="nopage-container">
+        <div className="text-center pt-20">
+          <h2>Uh oh! lost track?</h2>
+          <Link to="/">Click here to go to home page</Link>
+        </div>
+      </body>
     </>
   );
 };
